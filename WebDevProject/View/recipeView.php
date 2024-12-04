1478ef5f48cfaebc16fcafbc1c44b240905eefd7
amaebo <amaebong20033@gmail.com>
2024-12-03 22:37:17 -0700

    Delete files

--- WebDevProject/View/recipeView.php
+++ WebDevProject/View/recipeView.php
@@ -1,45 +1,7 @@
 <?php 
-/*$dsn = 'mysql:host=localhost; dbname=payroll';
-$username = 'employee_manager';
-$password = 'pa55word';
-
-try {
-$db = new PDO($dsn, $username, $password);
-} catch (PDOException $e) {
-$error_message = $e->getMessage();
-echo '<p>An error occurred while connecting to
-the database: $error_message </p>';
-}*/
-/*
-$recipes = [
-    $recipe1 =[
-        "RECIPE_NAME" => "Braised Beef",
-        "RECIPE_TYPE" => "Breakfast",
-        "RECIPE_COOK_TIME" => "2 hours",
-        "RECIPE_CALORIES" => "640",
-        "RECIPE_SERVINGS" => "8",
-    "RECIPE_IMAGE" => "./Songs_in_the_key_of_life.jpg",],
-        $recipe3 =[
-            "RECIPE_NAME" => "Braised Beef",
-            "RECIPE_TYPE" => "Breakfast",
-            "RECIPE_COOK_TIME" => "2 hours",
-            "RECIPE_CALORIES" => "640",
-            "RECIPE_SERVINGS" => "8",
-        "RECIPE_IMAGE" => "./SabaBLP.jpg",],
-    $recipe2 =[
-        "RECIPE_NAME" => "Braised Beef",
-        "RECIPE_TYPE" => "Breakfast",
-        "RECIPE_COOK_TIME" => "2 hours",
-        "RECIPE_CALORIES" => "640",
-        "RECIPE_SERVINGS" => "8",
-        "RECIPE_IMAGE" => "./candydrip.jpg",]
-
-];
-*/
-
-
+include 'pageHeader.php'
 ?>
-<?php include 'pageHeader.php' ?>
+
 <head>
     <title>Recipe List</title>
     <link rel="stylesheet" href="../recipe_table.css" />
